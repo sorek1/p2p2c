b2588fac36b9397055f68a955b2879161f58c86b
Nathan Rajlich <n@n8.io>
2018-08-09 18:16:43 -0700

    Bind to "0.0.0.0" interface (#23)
    This is required now.

--- crystal-hello/main.cr
+++ crystal-hello/main.cr
@@ -5,6 +5,6 @@ server = HTTP::Server.new do |context|
   context.response.print "Hello world! The time is #{Time.now}"
 end
 
-address = server.bind_tcp 3000
+address = server.bind_tcp "0.0.0.0", 3000
 puts "Listening on http://#{address}"
 server.listen
