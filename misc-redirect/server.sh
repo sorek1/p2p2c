2b4a907e8d6905593ffcde01f85f590e97a2a31a
Nathan Rajlich <n@n8.io>
2018-08-18 18:51:29 -0700

    misc-redirect: fix usage with absolute base URLs (#44)
    Fixes #43.

--- misc-redirect/server.sh
+++ misc-redirect/server.sh
@@ -7,5 +7,16 @@ if [ -z "${REDIRECT_URL-}" ]; then
   exit 2
 fi
 
+# Strip the trailing `/` from the base redirect URL, if there is one
+base_url="${REDIRECT_URL%%/}"
+
+# If the root URL was requested, don't append the beginning slash to the
+# final redirect URL. Anything else gets appended to the base URL.
+# (This is to make absolute base URLs with query-strings work as expected)
+redirect_url=
+if [ "$REQUEST_URI" != "/" ]; then
+  redirect_url="$REQUEST_URI"
+fi
+
 set_response_code "${STATUS-301}"
-set_response_header "Location" "${REDIRECT_URL%%/}$REQUEST_URI"
+set_response_header "Location" "$base_url$redirect_url"
