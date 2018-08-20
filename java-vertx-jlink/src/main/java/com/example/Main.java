fc8b428d06a8dddce21592ec97d6c51ed88dab72
tofflos <erik.ostlund@gmail.com>
2018-08-20 23:16:38 +0200

    Add Java Vert.x example (#46)

---
+++ java-vertx-jlink/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import io.vertx.core.Vertx;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        Vertx.vertx().createHttpServer().requestHandler(request -> request.response().end("Hello world!")).listen(8080);
+        LOGGER.info("Server listening on port 8080.");
+    }
+}
