2b4c0530cdaf2faa8edb914da9647a717976561c
Joe Cohen <joecohens@users.noreply.github.com>
2018-09-06 01:07:50 -0500

    Add Nuxt.js server-side rendered example (#65)
    * add nuxt ssr example
    
    * remove build files
    
    * ignore build files
    
    * update node-nuxt.js readme

---
+++ node-nuxt.js/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        Nuxt.js SSR Example
+      </h1>
+      <div class="links">
+        <nuxt-link
+          to="about"
+          class="button--green">Navigate to About</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+h1 {
+  padding-top: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
