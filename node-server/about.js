e45dd35af09fa042bb67bc5e4a846e2932418df1
Joe Cohen <joecohens@users.noreply.github.com>
2018-12-20 16:19:22 -0600

    Add more routes to node-server (#204)
    * Add more routes to node-server
    
    * Simplify builders for both routes

--- node-server/about.js
+++ node-server/about.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHeader(200, {"Content-Type": "text/html"});
-  res.end('This is a Node.js server running on Now 2.0 <hr> <a href="/about">Navigate to About Page</a>');
+  res.end('This is the About Page <hr> <a href="/">Navigate to Home</a>');
 });
 
 server.listen();
