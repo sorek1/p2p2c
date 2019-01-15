7a43e3bbd9734f7043156d1e29299ceba39d1902
Tejas Kumar <hello@tej.as>
2019-01-15 15:27:51 +0100

    Fix cookie session middleware (#228)
    Fixes #227. Thanks again, @zbycz! 💞

--- express/middlewares/cookieSession.js
+++ express/middlewares/cookieSession.js
@@ -3,7 +3,7 @@ const cookieSession = require("cookie-session");
 module.exports = cookieSession({
   name: "user-from-twitter",
   keys: [process.env.COOK_KEY],
-  domain: "serverless-express.now.sh"
+  domain: "serverless-express.now.sh",
 
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000, // 24 hours
