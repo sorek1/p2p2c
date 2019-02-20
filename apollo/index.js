8117af1ca2847b76731a32fe10b894dce7bd965a
Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
2019-02-21 00:51:16 +0100

    Add concrete example for Apollo (#273)
    * Add concrete example
    
    * Add > Success! Initialized "apollo" example in ~/source/zeit-workspace/now-examples/apollo/apollo. command to the bottom
    
    * Improve Readme
    
    * Delete wrong files
    
    * Remove uncessary sentence

--- apollo/index.js
+++ apollo/index.js
@@ -1,18 +1,87 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require('apollo-server');
+const { find, filter } = require('lodash');
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
+const books = [
+  { id: 1, title: 'The Trials of Brother Jero', rating: 8, authorId: 1 },
+  { id: 2, title: 'Half of a Yellow Sun', rating: 9, authorId: 3 },
+  { id: 3, title: 'Americanah', rating: 9, authorId: 3 },
+  { id: 4, title: 'King Baabu', rating: 9, authorId: 1 },
+  { id: 5, title: 'Children of Blood and Bone', rating: 8, authorId: 2 },
+];
+
+const authors = [
+  { id: 1, firstName: 'Wole', lastName: 'Soyinka' },
+  { id: 2, firstName: 'Tomi', lastName: 'Adeyemi' },
+  { id: 3, firstName: 'Chimamanda', lastName: 'Adichie' },
+];
+
+
+const typeDefs = `
+  type Author {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    books: [Book]!  # the list of books by this author
+  }
+
+  type Book {
+    id: Int!
+    title: String!
+    rating: Integer!
+    author: Author!
+  }
+
   type Query {
-    hello: String
+    books: [Book!]!,
+    book(id: Int!): Book!
+    author(id: Int!): Author!
+  }
+
+  # the schema allows the following query
+  type Query {
+    books: [Book!]!,
+    book(id: Int!): Book!
+    author(id: Int!): Author!
+  }
+
+  # this schema allows the following mutation
+  type Mutation {
+    addBook(title: String!, rating: Integer!, authorId: Int!): Book!
   }
 `;
 
-// Provide resolver functions for your schema fields
+let bookId = 5;
+let authorId = 3;
+
 const resolvers = {
   Query: {
-    hello: () => "Hello world from ApolloServer on Now 2.0!"
-  }
+    books: () => books,
+    book: (_, { id }) => find(books, { id: id }),
+    author: (_, { id }) => find(authors, { id: id }),
+  },
+  Mutation: {
+    addBook: (_, {title, rating, authorId }) => {
+      bookId++;
+
+      const newBook = {
+        id: bookId,
+        title,
+        rating,
+        authorId
+      };
+
+      books.push(newBook);
+      return newBook;
+    }
+  },
+  Author: {
+    books: (author) => filter(books, { authorId: author.id }),
+  },
+  Book: {
+    author: (book) => find(authors, { id: book.authorId }),
+  },
 };
+
 
 const server = new ApolloServer({
   typeDefs,
@@ -22,5 +91,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`)
 });
